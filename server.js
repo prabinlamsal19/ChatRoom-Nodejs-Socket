4613c2d227d268bcb1a7b98478b498f737ab8a33
prabinlamsal19 <lamsalprabin74@gmail.com>
2023-08-09 14:06:17 +0545

    chore: removed branch

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const http = require('http');
 const express = require('express')
-
+const formatMessage = require('./utils/messages')
 const socketio = require('socket.io');
 
 const app = express();
@@ -12,21 +12,22 @@ const io = socketio(server);
 //by default, on the home ,this shows the index.html file
 app.use(express.static(path.join(__dirname, 'public')));
 const PORT = process.env.PORT || 3000;
+const botname = "ChatCord"
 
 //this is going to run when a client makes a socket request or smth like that
 io.on('connection', socket => {
     console.log(`New Web Socket connection...`);
-    socket.emit('message', "Welcome To ChatCord!. (SOCKET EMIT)");
+    socket.emit('message', formatMessage(botname, "Welcome To ChatCord!. (SOCKET EMIT)"));
     //this message is sent to the function. MSG1
     //accessed on js/main.js  
     //only provided to the user that is connecting 
 
     //broadcast when a user connects(to all except the user who is connecting)
-    socket.broadcast.emit('message', 'A user has joined the chat(SOCKET BROADCAST EMIT');
+    socket.broadcast.emit('message', formatMessage(botname, 'A user has joined the chat(SOCKET BROADCAST EMIT'));
 
     //runs when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat (IO EMIT)');
+        io.emit('message', formatMessage('USER', 'A user has left the chat (IO EMIT)'));
     });
 
     //listen for chatMessages 
